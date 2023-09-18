d73e5c8059aaa35e4eab9ddfe1b4218179aca18e
AbishShreedharan <ashreedharan@r1rcm.com>
2023-09-18 08:12:02 -0400

    added unit test

---
+++ EduRate.Api/Tests/InteractionControllerTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Xunit;
+using Moq;
+using EduRate.Api.Interfaces;
+using EduRate.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduRate.Api.Tests
+{
+    public class InteractionControllerTests
+    {
+        private readonly InteractionController _controller;
+        private readonly Mock<IInteractionService> _interactionServiceMock;
+
+        public InteractionControllerTests()
+        {
+            _interactionServiceMock = new Mock<IInteractionService>();
+            _controller = new InteractionController(_interactionServiceMock.Object);
+        }
+
+        [Fact]
+        public void UpvoteComment_ReturnsOkResult_WhenUpvoteIsSuccessful()
+        {
+            // Arrange
+            _interactionServiceMock.Setup(service => service.UpvoteComment(It.IsAny<int>())).Returns(true);
+
+            // Act
+            var result = _controller.UpvoteComment(1);
+
+            // Assert
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public void UpvoteComment_ReturnsBadRequest_WhenUpvoteIsUnsuccessful()
+        {
+            // Arrange
+            _interactionServiceMock.Setup(service => service.UpvoteComment(It.IsAny<int>())).Returns(false);
+
+            // Act
+            var result = _controller.UpvoteComment(1);
+
+            // Assert
+            Assert.IsType<BadRequestResult>(result);
+        }
+
+        [Fact]
+        public void DownvoteComment_ReturnsOkResult_WhenDownvoteIsSuccessful()
+        {
+            // Arrange
+            _interactionServiceMock.Setup(service => service.DownvoteComment(It.IsAny<int>())).Returns(true);
+
+            // Act
+            var result = _controller.DownvoteComment(1);
+
+            // Assert
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public void DownvoteComment_ReturnsBadRequest_WhenDownvoteIsUnsuccessful()
+        {
+            // Arrange
+            _interactionServiceMock.Setup(service => service.DownvoteComment(It.IsAny<int>())).Returns(false);
+
+            // Act
+            var result = _controller.DownvoteComment(1);
+
+            // Assert
+            Assert.IsType<BadRequestResult>(result);
+        }
+
+        [Fact]
+        public void GetCommentVotes_ReturnsOkResult_WithVotes()
+        {
+            // Arrange
+            var expectedVoteCount = 10;
+            _interactionServiceMock.Setup(service => service.GetCommentVotes(It.IsAny<int>())).Returns(expectedVoteCount);
+
+            // Act
+            var result = _controller.GetCommentVotes(1) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            var responseObject = result.Value as dynamic;
+            Assert.Equal(expectedVoteCount, (int)responseObject.votes);
+        }
+    }
+}
---
+++ EduRate.Api/Tests/ReviewControllerTests.cs
@@ -0,0 +1,61 @@
+﻿using EduRate.Api.Controllers;
+using EduRate.Api.Interfaces;
+using EduRate.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+public class ReviewControllerTests
+{
+    private readonly ReviewController _controller;
+    private readonly Mock<IReviewService> _serviceMock = new Mock<IReviewService>();
+
+    public ReviewControllerTests()
+    {
+        _controller = new ReviewController(_serviceMock.Object);
+    }
+
+    [Fact]
+    public void AddComment_ValidComment_ReturnsOk()
+    {
+        _serviceMock.Setup(service => service.AddComment(It.IsAny<Comment>())).Returns(true);
+
+        var result = _controller.AddComment(new Comment());
+
+        Assert.IsType<OkResult>(result);
+    }
+
+    [Fact]
+    public void GetAllCommentsForModule_ValidModuleId_ReturnsOkWithComments()
+    {
+        var expectedComments = new List<Comment>
+        {
+            new Comment { /* Some properties here */ },
+            // Add more dummy comments as needed
+        };
+
+        _serviceMock.Setup(service => service.GetAllCommentsForModule(It.IsAny<int>())).Returns(expectedComments);
+
+        var result = _controller.GetAllCommentsForModule(1);
+
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.Equal(expectedComments, okResult.Value);
+    }
+
+    [Fact]
+    public void GetRecentComments_ValidModuleId_ReturnsOkWithComments()
+    {
+        var expectedComments = new List<Comment>
+        {
+            new Comment { /* Some properties here */ },
+            // Add more dummy comments as needed
+        };
+
+        _serviceMock.Setup(service => service.GetRecentComments(It.IsAny<int>())).Returns(expectedComments);
+
+        var result = _controller.GetRecentComments(1);
+
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.Equal(expectedComments, okResult.Value);
+    }
+}
